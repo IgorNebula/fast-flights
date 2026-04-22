0b26d6baadfe6d05627b6fd48029b8eb2323eb05
Alena <160515773+IgorNebula@users.noreply.github.com>
2026-04-22 10:58:31 +0300

    new file:   fast flights/Presentation/Views/Multi_sity.xaml 	new file:   fast flights/Presentation/Views/Multi_sity.xaml.cs 	new file:   fast flights/Presentation/Views/One_way.xaml 	new file:   fast flights/Presentation/Views/Round_trip.xaml 	new file:   fast flights/Presentation/Views/Round_trip.xaml.cs 	new file:   fast flights/Presentation/Views/one_way.xaml.cs 	modified:   fast flights/fast_flights.csproj
    	modified:   fast flights/AppShell.xaml
    	modified:   fast flights/Presentation/Views/One_way.xaml
    	modified:   fast flights/Presentation/Views/Round_trip.xaml

---
+++ fast flights/Presentation/Views/Round_trip.xaml.cs
@@ -0,0 +1,9 @@
+namespace fast_flights.Presentation.Views;
+
+public partial class Round_trip : ContentPage
+{
+	public Round_trip()
+	{
+		InitializeComponent();
+	}
+}
---
+++ fast flights/Presentation/Views/one_way.xaml.cs
@@ -0,0 +1,9 @@
+namespace fast_flights.Presentation.Views;
+
+public partial class one_way : ContentPage
+{
+	public one_way()
+	{
+		InitializeComponent();
+	}
+}
